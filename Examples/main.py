b72d2c4dbea4c30ba0c1e43e607905bc2ec9f3a0
XULIM <67230458+XULIM@users.noreply.github.com>
2023-03-30 13:39:01 -0400

    Added examples for loops.

--- Examples/main.py
+++ Examples/main.py
@@ -12,28 +12,51 @@ print("a =", a)
 print("b =", b)
 
 # Addition
-c = a + b # Declaration of c, and assignment of c.
+c = a + b  # Declaration of c, and assignment of c.
 print("a + b =", c)
 
-# Subtraction 
-c = a - b # Assignment of c.
-print("a - b =", c) 
+# Subtraction
+c = a - b  # Assignment of c.
+print("a - b =", c)
 
 # Multiplication
-c = a * b # Assignment of c.
-print("a * b =", c) 
+c = a * b  # Assignment of c.
+print("a * b =", c)
 
 # Division
-c = a / b # Assignment of c.
-print("a / b =", c) 
+c = a / b  # Assignment of c.
+print("a / b =", c)
 
-a = 1 # Integer which means number.
-print(type(a)) 
-a = "Hello" # String which means word.
+a = 1  # Integer which means number.
 print(type(a))
-a = 3.14 # Float variable which means decimal number.
+a = "Hello"  # String which means word.
 print(type(a))
-a = True # Boolean variable which means True or False.
+a = 3.14  # Float variable which means decimal number.
 print(type(a))
-a = None # NoneType variable which means No value.
+a = True  # Boolean variable which means True or False.
 print(type(a))
+a = None  # NoneType variable which means No value.
+print(type(a))
+
+
+# * Loops
+
+# Example of a while loop:
+i = 9
+while i > 0:
+    print("Hello")
+    i -= 1
+
+# Example of a for loop:
+for i in range(9):
+    print("Hello")
+
+'''
+Here we see that the while loop is in the form of:
+    while (boolean statement): 
+        action
+
+whereas for loop is in the form of: 
+    for (iteration):
+        action
+'''
